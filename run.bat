6514749e12d2718182cec3189661defe29545971
hanxianzhai <hanxianzhai@gmail.com>
2014-05-09 17:55:40 +0800

    nas

--- run.bat
+++ run.bat
@@ -1,16 +1,15 @@
 @ECHO OFF
 IF EXIST java (
-	start "NXT NRS" java -cp nxt.jar;lib\*;conf nxt.Nxt
+	start "NAS NRS" java -cp nxt.jar;lib\*;conf nxt.Nxt
 ) ELSE (
 	IF EXIST "%PROGRAMFILES%\Java\jre7" (
-		start "NXT NRS" "%PROGRAMFILES%\Java\jre7\bin\java.exe" -cp nxt.jar;lib\*;conf nxt.Nxt
+		start "NAS NRS" "%PROGRAMFILES%\Java\jre7\bin\java.exe" -cp nxt.jar;lib\*;conf nxt.Nxt
 	) ELSE (
 		IF EXIST "%PROGRAMFILES(X86)%\Java\jre7" (
-			start "NXT NRS" "%PROGRAMFILES(X86)%\Java\jre7\bin\java.exe" -cp nxt.jar;lib\*;conf nxt.Nxt
+			start "NAS NRS" "%PROGRAMFILES(X86)%\Java\jre7\bin\java.exe" -cp nxt.jar;lib\*;conf nxt.Nxt
 		) ELSE (
 			ECHO Java software not found on your system. Please go to http://java.com/en/ to download a copy of Java.
 			PAUSE
 		)
 	)
 )
-
